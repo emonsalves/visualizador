c7a5a64fd5a9bc35bdebd734f14c9f190f01d3ac
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2024-09-17 15:27:24 -0300

    Update App.jsx and router configuration

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Visualizador, Home } from "../pages";
+import { Visualizador, Home, ErrorPage } from "../pages";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />,
+        errorElement: <ErrorPage />,
     },
     {
         path: "/visualizador",
         element: <Visualizador />,
+        errorElement: <ErrorPage />,
+        redirectTo: "/",
     },
 ]);
